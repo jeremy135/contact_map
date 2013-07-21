b95c866a83d5aadfd2659c01637053f6109390cd
Jeremy Jackson <blackgrow@yandex.ru>
2013-07-21 10:41:56 +0900

    replace jQuery with $

--- js/contact_map.js
+++ js/contact_map.js
@@ -1,16 +1,18 @@
 //Drupal.behaviors.contact_map = function (context) {
-jQuery(document).ready( function() {
-    jQuery('#plus a').click(function(e) {
-        jQuery('#myMap').attr('style', cm.maxSize);
-        jQuery('#plus').addClass('hide');
-        jQuery('#minus').removeClass('hide');
+function ($) {
+$(document).ready( function() {
+    $('#plus a').click(function(e) {
+        $('#myMap').attr('style', cm.maxSize);
+        $('#plus').addClass('hide');
+        $('#minus').removeClass('hide');
         e.preventDefault();
     })
-    jQuery('#minus a').click(function(e) {
-        jQuery('#myMap').attr('style', cm.minSize);
-        jQuery('#minus').addClass('hide');
-        jQuery('#plus').removeClass('hide');
+    $('#minus a').click(function(e) {
+        $('#myMap').attr('style', cm.minSize);
+        $('#minus').addClass('hide');
+        $('#plus').removeClass('hide');
         e.preventDefault();
     })
 })
+}(jQuery)
 //}
